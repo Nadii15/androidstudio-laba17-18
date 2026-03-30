b8df9dc4c5a4623c410827fc16e5e7e0f1c1d919
okolodk <grisha.zhivot@bk.ru>
2026-03-30 21:37:07 +0300

    Step 6: Create UI with Compose

--- app/src/main/java/com/ryckluk/androidstudio_laba17_18/data/WeatherData.kt
+++ app/src/main/java/com/ryckluk/androidstudio_laba17_18/data/WeatherData.kt
@@ -1,9 +1,9 @@
 package com.ryckluk.androidstudio_laba17_18.data
 
-class WeatherData(
-    val temperature: Int? = null,
-    val humidity: Int? = null,
-    val windSpeed: Int? = null,
+data class WeatherData(
+    val temperature: Int = 0,
+    val humidity: Int = 0,
+    val windSpeed: Int = 0,
     val isLoading: Boolean = false,
     val error: String? = null
 ) {
