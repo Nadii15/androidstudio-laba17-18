b23362397b20b998239dbd2c88bfcc524874db56
okolodk <grisha.zhivot@bk.ru>
2026-03-30 20:21:15 +0300

    Step 3: Create WeatherData model

---
+++ app/src/main/java/com/ryckluk/androidstudio_laba17_18/data/WeatherData.kt
@@ -0,0 +1,12 @@
+package com.ryckluk.androidstudio_laba17_18.data
+
+class WeatherData(
+    val temperature: Int? = null,
+    val humidity: Int? = null,
+    val windSpeed: Int? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+) {
+
+
+}
